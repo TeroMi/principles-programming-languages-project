5153f27da4384b87211b0916b0c614d81f73aca4
Tero Mielikäinen <terppa.mielikainen@hotmail.com>
2019-10-02 21:13:16 +0300

    Started to work on lexer, added command line arguments

---
+++ 01_lexer/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys, ply.lex
+if __name__ == '__main__':
+    import argparse, codecs
+    argument_parser = argparse.ArgumentParser()
+    argument_group = argument_parser.add_mutually_exclusive_group()
+    argument_group.add_argument('--who', action='store_true', help='author')
+    argument_group.add_argument('-f','--file', help='input filename for lexical analysis')
+
+    input_args_parsed = argument_parser.parse_args()
+    if input_args_parsed.who:
+        print("Author: 283121 Tero Mielikäinen")
+    elif input_args_parsed.file is None:
+        argument_parser.print_help()
